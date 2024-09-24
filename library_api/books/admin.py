6efefb80a2b035e4ab05e49c11231406b0e16a9c
Fahad Shawal <fahadshawal@gmail.com>
2024-09-24 12:52:38 +0500

    register models to admin portal

--- library_api/books/admin.py
+++ library_api/books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, FavoriteBook
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'published_date']
+    search_fields = ['title', 'author__name']
+    list_filter = ['published_date', 'author']
+
+@admin.register(FavoriteBook)
+class FavoriteBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'book']
+    search_fields = ['user__username', 'book__title']
